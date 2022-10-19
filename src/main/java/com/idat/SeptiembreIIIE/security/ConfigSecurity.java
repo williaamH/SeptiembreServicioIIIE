b3df5a112623f95b96114541711b95a410c88b37
williaamH <william_03_07@hotmail.com>
2022-10-19 00:15:23 -0500

    Semana 6

--- src/main/java/com/idat/SeptiembreIIIE/security/ConfigSecurity.java
+++ src/main/java/com/idat/SeptiembreIIIE/security/ConfigSecurity.java
@@ -1,62 +1,113 @@
 package com.idat.SeptiembreIIIE.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 //import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 //import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ConfigSecurity extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private UserDetailService service;
+	
+	@Autowired
+	private TokenFilter filter;
+	
+	@Autowired
+	private EntryPoint entrypoint;
+
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManager();
+	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("william").password(Encoder().encode("123")).roles("USER","ADMIN");
-		auth.inMemoryAuthentication().withUser("william2").password(Encoder().encode("321")).roles("USER");
+//		auth.inMemoryAuthentication().withUser("PROFESOR").password(encriptado().encode("123")).roles("ADMIN");
+//		auth.inMemoryAuthentication().withUser("ALUMNO").password(encriptado().encode("123")).roles("USER");
+		auth.userDetailsService(service).passwordEncoder(encriptado());
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//			.antMatchers("/producto/v1/guardar").access("hasRole('ADMIN')")
+//			.antMatchers("/producto/v1/listar").permitAll()
+//			.and()
+//			.httpBasic()
+//			.and()
+//			.csrf().disable();
+		
 		http.authorizeRequests()
-		.antMatchers("/producto/v1/listar").permitAll()
-		.antMatchers("/item/v1/listar").permitAll()
-		.antMatchers("/producto/v1/**").access("hasRole('ADMIN')")
-		.and().httpBasic().and().csrf().disable();
+			.antMatchers("/crearToken").permitAll()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.exceptionHandling()
+			.authenticationEntryPoint(entrypoint)
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+			.csrf().disable();
+		
 	}
+
 	
-	@Bean public PasswordEncoder Encoder() {
+//	@Bean
+//	public UserDetailsService userDetail() {
+//		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//		manager.createUser(
+//				User.withUsername("profesor")
+//				.password(encriptado().encode("123"))
+//				.roles("ADMIN")
+//				.build());
+//		return manager;
+//	}
+//	
+	@Bean
+	public PasswordEncoder encriptado() {
 		return new BCryptPasswordEncoder();
-	 }
-	
-	/*
-	 * @Bean public UserDetailsService userDetail() { InMemoryUserDetailsManager
-	 * manager = new InMemoryUserDetailsManager(); manager.createUser( User
-	 * .withUsername("jesus") .password(Encoder().encode("123456"))
-	 * .roles("ADMIN").build() ); return manager; }
-	 * 
-	 * 
-	 * 
-	 * 
-	 * public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	 * http.authorizeRequests()
-	 * .antMatchers("/productos/v1/*").access("hasRoles('ADMIN')") .and()
-	 * .httpBasic() .and() .csrf().disable(); return http.build(); }
-	 */
-	
-	
-	
-	
+	}
+
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//		
+//		http.authorizeRequests()
+//			.antMatchers("/producto/v1/*").access("hasRole('ADMIN')")
+//			.and()
+//			.httpBasic()
+//			.and()
+//			.csrf().disable();
+//		
+//		return http.build();
+//	}
 	
 }
