eb45b570d445e8f697eed4b10e4498130dabac10
williaamH <william_03_07@hotmail.com>
2022-11-10 15:11:04 -0500

    la cague

--- src/main/java/com/idat/SeptiembreIIIE/security/UserDetailService.java
+++ src/main/java/com/idat/SeptiembreIIIE/security/UserDetailService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +16,7 @@ public class UserDetailService implements UserDetailsService {
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
 		if( "william".equals(username)) {
-			return new User("william", "123", new ArrayList<>());
+			return new User("william", new BCryptPasswordEncoder().encode("123"), new ArrayList<>());
 		}else {
 			throw new UsernameNotFoundException("Usuario no existe " + username);
 		}
