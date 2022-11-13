f60c6b21d434b5234087f7d172d38801f5d6d0b4
williaamH <william_03_07@hotmail.com>
2022-11-12 19:32:49 -0500

    Semana 8

--- src/main/java/com/idat/SeptiembreIIIE/model/Producto.java
+++ src/main/java/com/idat/SeptiembreIIIE/model/Producto.java
@@ -1,12 +1,54 @@
 package com.idat.SeptiembreIIIE.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="productos")
 public class Producto {
 
+	@Id()
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_PRODUCTO")
 	private Integer idProducto;
 	private String nombreProducto;
 	private String descripcion;
 	private Double precio;
 	private Integer Stock;
+	
+	@OneToOne
+	private Proveedor proveedor;
+	
+	@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(
+			name = "producto_cliente",
+			joinColumns = @JoinColumn(
+					name = "id_producto",
+					nullable = false,
+					unique = true,
+					foreignKey = @ForeignKey(foreignKeyDefinition = "foreign key(id_producto) references productos(id_producto)")
+				),
+			inverseJoinColumns = @JoinColumn(
+					name = "id_cliente",
+					nullable = false,
+					unique = true,
+					foreignKey = @ForeignKey(foreignKeyDefinition = "foreign key(id_cliente) references clientes(id_cliente)")
+				)
+			)
+	private List<Cliente> clientes = new ArrayList<>();
 	
 	public Producto() {
 		
